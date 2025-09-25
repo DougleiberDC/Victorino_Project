abac75314c67fc1be18b806a2793f105d5c4e680
Dougleiber Correa <dougleibercorrea@gmail.com>
2025-09-25 17:41:44 -0400

    Delete victorino directory

--- victorino/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Biblioteca Hospital Victorino Santaella</p>
-        </div>
-    </footer>
-</body>
-</html>
--- victorino/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'conexion.php';
-
-if ($mysqli->connect_error) {
-    die("Error de conexión: " . $mysqli->connect_error);
-} else {
-    echo "¡Conexión exitosa a la base de datos!";
-}
-?>
